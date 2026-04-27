2c99e58f382626b8dd7ad6175a58d2e72bf10f36
Chandan.Kumar <chandan04051999@gmail.com>
2026-04-27 22:55:33 +0530

    production bug fixes

--- vite.config.js
+++ vite.config.js
@@ -5,39 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (!id.includes("node_modules")) return;
-
-          if (
-            id.includes("three") ||
-            id.includes("@react-three/fiber") ||
-            id.includes("@react-three/drei") ||
-            id.includes("@react-spring/three")
-          ) {
-            return "three-vendor";
-          }
-
-          if (
-            id.includes("react-router-dom") ||
-            id.includes("@remix-run/router")
-          ) {
-            return "router-vendor";
-          }
-
-          if (id.includes("react-toastify")) {
-            return "toast-vendor";
-          }
-
-          if (id.includes("react") || id.includes("scheduler")) {
-            return "react-vendor";
-          }
-
-          return "vendor";
-        },
-      },
-    },
-  },
 });
